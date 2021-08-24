1d8e1d9aab7598f03ac648dc39239a589e355c3d
MAVINGAyannis <maving_y@etna-alternance.net>
2021-08-24 18:45:49 +0200

    Collection view created

---
+++ Movie in theatres/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Movie in theatres
+//
+//  Created by Yannis Mavinga on 24/08/2021.
+//
+import SwiftUI
+
+struct DataModel: Identifiable {
+let id: String
+let name: String
+let imageName: String
+}
+
+struct ContentView: View {
+    let data: [DataModel] = [
+    .init(id: "0", name: "Chat", imageName: "Cat"),
+    .init(id: "1", name: "Chien", imageName: "Dog"),
+    .init(id: "2", name: "Oiseau", imageName: "Bird"),
+    .init(id: "3", name: "Cheval", imageName: "Horse")
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(data) { items in
+                    ForEach(0..<1) { item in
+                        CollectionView(data: items)
+                    }
+                }
+            }.navigationBarTitle("Movie in theatres", displayMode: .inline)
+        }
+    }
+}
+
+struct CollectionView: View {
+    let data: DataModel
+    var body: some View {
+        VStack {
+            HStack {
+                    Spacer()
+                    Image(self.data.imageName)
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(.yellow)
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+                    Spacer()
+                .padding(.bottom, 16)
+            }
+            HStack {
+                Spacer()
+                Text(self.data.name)
+                Spacer()
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.colorScheme, .light)
+    }
+}
---
+++ Movie in theatres/Movie_in_theatresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_in_theatresApp.swift
+//  Movie in theatres
+//
+//  Created by Yannis Mavinga on 24/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Movie_in_theatresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
